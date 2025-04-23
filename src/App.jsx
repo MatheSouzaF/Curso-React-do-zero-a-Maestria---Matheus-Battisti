3fc32115ee4459ef13ea63d0dc590098617173fb
matheussf <matheus@dzigual.com.br>
2025-04-23 17:51:10 -0300

    feat: add FirstComponent and update App to use it

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
+import FirstComponent from "./components/FirstComponent";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Hello World</h1>
+      <FirstComponent />
     </>
   );
 }
---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,9 @@
+const FirstComponent = () => {
+  return (
+    <div>
+      <h1>Matheus</h1>
+      <p>This is the first component of our React application.</p>
+    </div>
+  );
+};
+export default FirstComponent;
